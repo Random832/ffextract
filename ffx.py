251202644121fe0c97535ca87ffe06dfd2df2fe8
Random832 <random832@fastmail.com>
2021-02-13 00:49:41 -0500

    Try not to extract invalid files
    FF has no magic numbers, but there are clear structural problems that are very likely to appear when fed arbitrary bytes.

--- ffx.py
+++ ffx.py
@@ -21,28 +21,45 @@ if opts.dir:
     os.chdir(opts.dir)
 
 def readint():
-    return int.from_bytes(ff.read(4), 'little')
-
-count = readint()
-lst = []
-for i in range(count-1):
-    offset = readint()
-    name = ff.read(13).decode('oem').strip('\0')
-    if '/' in name or '\\' in name:
-        name = name.replace('/', '_').replace('\\', '_')
-    lst.append((offset, name))
-eoffset = readint()
-lst.append((eoffset, None))
+    data = ff.read(4)
+    if len(data) < 4: raise EOFError
+    return int.from_bytes(data, 'little') & 0xFFFFFFFF
 
 def ismatch(fn):
     return any(fnmatch.fnmatch(fn, pat) for pat in opts.files or ['*'])
 
+count = readint()
+print('archive contains', count - 1, 'files')
+if count <= 0 or count > 1048576:
+    print('archive appears invalid, exiting')
+    sys.exit(1)
+if count == 1: # could be a valid empty archive ¯\_(ツ)_/¯
+    sys.exit(0)
+
+lst = []
+for i in range(count):
+    offset = readint()
+    name = ff.read(13).decode('oem').strip('\0')
+    if len(name) > 12:
+        print('archive appears invalid, exiting')
+        sys.exit(1)
+    if lst:
+        offset1, name1 = lst[-1]
+        if offset1 > offset:
+            print('archive appears invalid, exiting')
+            sys.exit(1)
+        if ismatch(name1):
+            print(name1, offset-offset1)
+    lst.append((offset, name))
+if(lst[-1][1] != ''):
+    print('unexpected filename', lst[-1][1], 'for final directory entry')
+
 for ((offset, name), (offset2, _)) in zip(lst, lst[1:]):
     size = offset2-offset
-    if ismatch(name):
-        print(name, size)
     if opts.mode == 'x':
         #ff.seek(offset)
+        if '/' in name or '\\' in name:
+            name = name.replace('/', '_').replace('\\', '_')
         data = ff.read(offset2-offset)
         if ismatch(name):
             with open(name, 'wb') as of:
